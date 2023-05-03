75767f56188782129377b457dd13c0e34b18aa39
Meraj al Maksud <merajmasuk@gmail.com>
2023-05-03 22:11:03 +0600

    character script added

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -4,7 +4,8 @@ extends KinematicBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var velocity = Vector3(0, 0, 0)
+const SPEED = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +15,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.1)
+	
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	else:
+		velocity.z = lerp(velocity.z, 0, 0.1)
+	
+	move_and_slide(velocity)
